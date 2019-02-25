0b72dc45c44bf54d635dbf16cdbbdf59c21838ed
Alejandro Gonzalez Alonso <alu0100997910@ull.edu.es>
2019-02-25 17:21:40 +0000

    added extension view generator and open link functionality

--- extension.js
+++ extension.js
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const opn = require('opn');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -9,7 +10,6 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "webbookmarks" is now active!');
@@ -17,11 +17,28 @@ function activate(context) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
+	let disposable = vscode.commands.registerCommand('extension.openWebBookmarks', function () {
 		// The code you place here will be executed every time your command is executed
-
+		let bookmarks = {
+			"Google": "https://www.google.es",
+			"Amazon": "https://www.amazon.es"
+		}
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		//vscode.window.showInformationMessage('Hello World!');
+		const panel = vscode.window.createWebviewPanel('webBookmarks', 'Web Bookmarks', vscode.ViewColumn.One, {
+			enableScripts: true
+		});
+		panel.webview.html = constructHtml(bookmarks);
+
+		panel.webview.onDidReceiveMessage(message => {
+			switch (message.command) {
+				case 'open':
+					vscode.window.showInformationMessage('Abriendo Navegador...');
+					console.log(message);
+					opn(message.url);
+					return;
+			}
+		}, undefined, context.subscriptions)
 	});
 
 	context.subscriptions.push(disposable);
@@ -29,7 +46,43 @@ function activate(context) {
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() { }
+
+function constructHtml(bookmarks) {
+	let table = "<table>";
+	for (let key in bookmarks) {
+		table += `<tr><td>${key}</td><td><button class="link" value="${bookmarks[key]}">Enlace</button></td></tr>`;
+	}
+	table += "</table>";
+	return `
+	<!DOCTYPE html>
+	<html>
+		<head>
+			<meta charset="UTF-8">
+			<title>Web Bookmarks</title>
+		</head>
+		<body>
+			<h1 id="h1"> Hello World </h1>
+			${table}
+
+			<script>
+				window.onload = function(){
+					const vscode = acquireVsCodeApi();
+					let link = document.querySelectorAll(".link");
+					for(let i of link){
+						i.addEventListener('click', function(){
+							console.log("click", i.value);
+							vscode.postMessage({
+								command: 'open',
+								url: i.value
+							});
+						},false);
+					}
+				}
+			</script>
+		</body>
+	</html>`;
+}
 
 module.exports = {
 	activate,
